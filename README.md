644d974877354c408ac6400a640c619f3297fca7
Xenostar <157074608+Xenostar1337@users.noreply.github.com>
2024-02-09 19:07:15 -0700

    Beta release of Calculator

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # calculatorPage
 Calculator for The Odin Project
 
-2/8/24 started calculator process.  
-Base idea is to create layout, have the buttons populate a string. Use the string to display button presses. Populate an array with previous string operation.
-Pressing C will pop the last item in the array, CE will clear Array.
-Pressing = will evaluate the displayed operation so will trying to apply a 2nd operator +-*/ with a valid operation already displayed. ie 33 + 33 / 2  the / will trigger evaluating the valid operation so 66 will be evaluated by the / button.
+2/8/24 
+    started calculator process.  
+    Base idea is to create layout, have the buttons populate a string. Use the string to display button presses. Populate an array with previous string operation.
 
-foreseeable problems; decimals, incomplete operations, 0 tomfoolery. 
+    Pressing C will pop the last item in the array, CE will clear Array.
+
+    Pressing = will evaluate the displayed operation so will trying to apply a 2nd operator +-*/ with a valid operation already 
+    displayed. ie 33 + 33 / 2  the / will trigger evaluating the valid operation so 66 will be evaluated by the / button.
+
+    Blue buttons need to call a calculate function if the display has a operation.  check for existing operator or decimal in array.
+
+    Orange buttons should delete the display, or backspace
+
+    Foreseeable problems; decimals, incomplete operations, 0 tomfoolery. 
+
+
+2/9/24  
+
+    adjusted size for mobile, finished button functions. 
+    released beta for testing.
--- main.js
+++ main.js
@@ -1,71 +1,172 @@
+//<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/9.4.4/math.min.js"></script>
+
+let displayView = document.getElementsByClassName('display')[0];
+let overflowView = document.getElementsByClassName('overflow')[0];
+let operatorView = document.getElementsByClassName('operator')[0];
+const minus = "-";
+const add = "+";
+const divide = "÷";
+const mult = "×";
+
 for (let i = 1; i <= 20; i++) {
     let buttonId = 'button' + i;
     let currentButton = document.getElementById(buttonId);
 
-    var displayView = document.getElementsByClassName('display')[0];
 
-
-    function clear(){
-        displayView.textContent = "";
-    };
-
-    // Perform different actions based on which button is clicked
+// Button Actions        
     if (currentButton) { 
         displayView.textContent = "";
-        currentButton.addEventListener('click', function() {
-            
-            
-            console.log("Button " + i + " clicked!"); 
+        currentButton.addEventListener('click', function() {         
             switch (i) {
-            case 1:
-                console.log("Action for Button smile");
-                // Perform action for Smile
+//special                
+            case 1:  
+               displayView.textContent = "8008135";              
                 break;
-            case 2:
-                console.log("Action for Button pi");
-                displayView.textContent = '3.1415926535897';
+//pi
+            case 2: 
+                displayView.textContent = '3.141592653589';
                 break;
-            case 3:
-                console.log("Action for Button clear");
+//clear
+            case 3: 
                 displayView.textContent = "";
+                operatorView.textContent = "";
+                overflowView.textContent = "";
                 break;
-            case 4:
-                console.log("Action for Button bckspce");
-                // Perform action for backspace
+//backspace                
+            case 4: 
+                let modifiedString = displayView.textContent.slice(0, -1);
+                  displayView.textContent = modifiedString;
                 break;
-            case 8:
-                console.log("Action for Button -");
-                // Perform action for minus
+// minus                
+            case 8:                 
+                if (operatorView.textContent !== "" && displayView.textContent !== "") {
+                    solve();   
+                    operatorView.textContent = minus;
+                    overflowView.textContent = displayView.textContent; 
+                    displayView.textContent = "";   
+                } else if (operatorView.textContent !== "") {
+                    operatorView.textContent = minus;
+                } else if (operatorView.textContent !== "" && displayView.textContent !==""){
+                    solve();
+                }else if (operatorView.textContent == "") {
+                    overflowView.textContent = displayView.textContent;                        
+                    displayView.textContent = "";
+                    operatorView.textContent = minus;
+                }  
                 break;
-            case 12:
-                console.log("Action for Button /");
-                // Perform action for divide
+// divide                
+            case 12: 
+                if (operatorView.textContent !== "" && displayView.textContent !== "") {
+                    solve();   
+                    operatorView.textContent = divide;
+                    overflowView.textContent = displayView.textContent; 
+                    displayView.textContent = "";   
+                } else if (operatorView.textContent !== "" && displayView.textContent === "") {
+                    operatorView.textContent = divide;
+                } else if (operatorView.textContent == "") { 
+                    overflowView.textContent = displayView.textContent;                        
+                    displayView.textContent = "";
+                    operatorView.textContent = divide;
+                } 
                 break;
-            case 16:
-                console.log("Action for Button x");
-                // Perform action for multiply
+// multiply        
+            case 16: 
+                if (operatorView.textContent !== "" && displayView.textContent !== "") {
+                    solve();    
+                    operatorView.textContent = mult;
+                    overflowView.textContent = displayView.textContent; 
+                    displayView.textContent = "";
+                } else if (operatorView.textContent !== "" && displayView.textContent ==="") {
+                    operatorView.textContent = mult;
+                } else if (operatorView.textContent !== "" && displayView.textContent !==""){
+                    solve();
+                } else if (operatorView.textContent == "") {
+                    overflowView.textContent = displayView.textContent;                        
+                    displayView.textContent = "";
+                    operatorView.textContent = mult;
+                } 
                 break;  
-            case 17:
-                console.log("Action for Button .");
-                // Perform action for decimal
+//decimal      
+            case 17: 
+                if (!displayView.textContent.includes(".")){
+                    displayView.textContent += ".";
+                }                 
                 break;
-            case 19:
-                console.log("Action for Button =");
-                // Perform action for equals
-            case 20:
-                console.log("Action for Button +");
-                // Perform action for plus
-                break;  
-            default:// default action
-                if (displayView.textContent.length < 15) {
-                    console.log("Default action");
-                    let testIt = document.getElementById(buttonId).textContent;
-                    displayView.textContent += testIt;    
-                    console.log(displayView.textContent.length)
-                    }
+//equals             
+            case 19:               
+                    solve();                             
+                break;
+//addition                
+            case 20: 
+                if (operatorView.textContent !== "" && displayView.textContent !== "") {
+                    solve();  
+                    operatorView.textContent = add;
+                    overflowView.textContent = displayView.textContent; 
+                    displayView.textContent = "";    
+                } else if (operatorView.textContent !== "") {
+                    operatorView.textContent = add;
+                } else if (operatorView.textContent !== "" && displayView.textContent !==""){
+                    solve();
+                }else if (operatorView.textContent == "") {
+                    overflowView.textContent = displayView.textContent;                        
+                    displayView.textContent = "";
+                    operatorView.textContent = add;
+                }  
+                break;
+// numbers                 
+            default:
+                if (displayView.textContent.length < 14) {
+                    let number = document.getElementById(buttonId).textContent;
+                    if (displayView.textContent == 0){
+                        displayView.textContent = number;
+                    } else{
+                        displayView.textContent += number;            
+                    }}
             }                  
         });
     }
 }
   
+function solve(){               
+    if (operatorView.textContent.includes(divide) && displayView.textContent == "0" ) {
+        let error = "Bad human!";
+        displayView.textContent = error;
+        setTimeout(byZero, 2000);
+    } else if( operatorView.textContent === minus){
+        let total = parseFloat(overflowView.textContent) - parseFloat(displayView.textContent);
+        let totalString = total.toString();
+        displayView.textContent = totalString.slice(0, 14);
+        overflowClear();
+    } else if (operatorView.textContent === add){
+        let var1 = (overflowView.textContent);
+        let var2 = (displayView.textContent);
+        let total = parseFloat(var1) + parseFloat(var2);
+        let totalString = total.toString();
+        displayView.textContent = totalString.slice(0, 14);
+        overflowClear();
+    } else if (operatorView.textContent === mult){
+        let total = parseFloat(overflowView.textContent) * parseFloat(displayView.textContent);
+        let totalString = total.toString();
+        displayView.textContent = totalString.slice(0, 14);
+        overflowClear();
+    } else if (operatorView.textContent === divide){
+        console.log(displayView.textContent);
+        console.log(typeof displayView.textContent);
+        let total = parseFloat(overflowView.textContent) / parseFloat(displayView.textContent);
+        let totalString = total.toString();
+        displayView.textContent = totalString.slice(0, 14);
+        overflowClear();
+    }
+        
+    
+}   
 
+function byZero (){
+    displayView.textContent = "";
+}
+
+
+function overflowClear(){
+        operatorView.textContent = "";
+        overflowView.textContent = "";
+}
